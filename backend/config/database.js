34eca745eb85d94af7489024ae8252952fea4031
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2025-03-07 00:07:21 +0530

    Update database.js
    add pool

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,17 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize('freedb_myOctalogic', 'freedb_octalogic-user', '5z5mRyU2&%A%ZdF', {
   host: 'sql.freedb.tech',
   dialect: 'mysql',
-  port: 3306, // ensure port is a number, not a string
+  port: 3306, // Ensure port is a number, not a string
   logging: false,
   dialectOptions: {
     connectTimeout: 10000, // Timeout for establishing a connection
   },
+  pool: {
+    max: 5,          // Maximum number of connections in the pool
+    min: 0,          // Minimum number of connections in the pool
+    acquire: 30000,  // Maximum time, in milliseconds, that pool will try to get a connection before throwing an error
+    idle: 10000      // Maximum time, in milliseconds, that a connection can be idle before being released
+  }
 });
 
 const connectDB = async () => {
